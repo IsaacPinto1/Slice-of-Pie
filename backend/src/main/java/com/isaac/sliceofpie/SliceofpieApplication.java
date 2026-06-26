8b690b76ee96758eac5d32fc888aac917be4391c
Isaac Pinto <isaac.pinto2003@gmail.com>
2026-06-25 20:17:47 -0700

    initial commit

---
+++ backend/src/main/java/com/isaac/sliceofpie/SliceofpieApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.sliceofpie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliceofpieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SliceofpieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/isaac/sliceofpie/SliceofpieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.sliceofpie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliceofpieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
