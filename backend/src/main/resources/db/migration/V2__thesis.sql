1ae5ba556c6f45d83714d753606646bf4ec111d9
Isaac Pinto <isaac.pinto2003@gmail.com>
2026-07-01 18:55:36 -0700

    idk

---
+++ backend/src/main/resources/db/migration/V2__thesis.sql
@@ -0,0 +1,22 @@
+CREATE TABLE thesis (
+    id BIGSERIAL PRIMARY KEY,
+
+    user_id BIGINT NOT NULL,
+    ticker VARCHAR(10) NOT NULL,
+
+    content TEXT NOT NULL,
+
+    created_at TIMESTAMP NOT NULL, 
+    updated_at TIMESTAMP NOT NULL 
+
+    CONSTRAINT fk_thesis_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT uq_user_ticker
+        UNIQUE (user_id, ticker)
+);
+
+CREATE INDEX idx_thesis_user_id ON thesis(user_id);
+CREATE INDEX idx_thesis_ticker ON thesis(ticker);
