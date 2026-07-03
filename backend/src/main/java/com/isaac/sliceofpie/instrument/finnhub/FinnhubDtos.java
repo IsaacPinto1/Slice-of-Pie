42c5471ae77cd6929a960719c0bfebd03d8feee7
Isaac Pinto <isaac.pinto2003@gmail.com>
2026-07-02 21:02:05 -0700

    remove tag

--- backend/src/main/java/com/isaac/sliceofpie/instrument/finnhub/FinnhubDtos.java
+++ backend/src/main/java/com/isaac/sliceofpie/instrument/finnhub/FinnhubDtos.java
@@ -3,7 +3,6 @@ package com.isaac.sliceofpie.instrument.finnhub;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class FinnhubDtos {
 
@@ -16,7 +15,7 @@ public class FinnhubDtos {
     @JsonIgnoreProperties(ignoreUnknown = true)
     record FinnhubSearchResult(
             String description,
-            @JsonProperty("displaySymbol") String displaySymbol,
+            String displaySymbol,
             String symbol,
             String type
     ) {}
